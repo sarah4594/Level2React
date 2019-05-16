ae236b2596e329d7e300c429b86521707c9aa4e7
Unknown <sarah4594@gmail.com>
2019-05-16 09:27:42 -0500

    Use children props to render

--- level2react/src/App.js
+++ level2react/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import Toggle from './ToggleRenderProps'
+import Toggle from './ToggleRPC'
 
 class App extends Component {
   render() {
@@ -11,14 +11,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Toggle
-          render={({ on, toggle }) => (
+        <Toggle>
+          {({ on, toggle }) => (
             <div>
               {on && <h1>Show Me</h1>}
               <button onClick={toggle}>Show/Hide</button>
             </div>
           )}
-        />
+        </Toggle>
       </div>
     )
   }
