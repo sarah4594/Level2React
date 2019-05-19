c5a399023101317ada6314508f2768e0547ee0a6
Unknown <sarah4594@gmail.com>
2019-05-19 17:14:12 -0500

    Create a portal using toggle component

--- level2react/src/App.js
+++ level2react/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg'
 import './App.css'
 import Toggle from './ToggleRPC'
 import Portal from './Portal'
+import Modal from './Modal'
 
 class App extends Component {
   render() {
@@ -15,9 +16,10 @@ class App extends Component {
         <Toggle>
           {({ on, toggle }) => (
             <Fragment>
-              {on && <h1>Show Me</h1>}
-              <button onClick={toggle}>Show/Hide</button>
-              <Portal>{on && <h1>I'm in a portal</h1>}</Portal>
+              <button onClick={toggle}>Login</button>
+              <Modal on={on} toggle={toggle}>
+                <h1>still in modal</h1>
+              </Modal>
             </Fragment>
           )}
         </Toggle>
