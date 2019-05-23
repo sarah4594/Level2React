bf79e73861a71167eaa593716e6cd3c5b111194f
Unknown <sarah4594@gmail.com>
2019-05-23 00:03:08 -0500

    Remove Modal and Icon components from Utilities

--- level2react/src/Utilities/index.js
+++ level2react/src/Utilities/index.js
@@ -1,8 +1,6 @@
 //export * from './Filename'
 
-import Icon from './Icon'
-import Modal from './Modal'
 import Portal from './Portal'
 import Toggle from './Toggle'
 
-export { Icon, Modal, Portal, Toggle }
+export { Portal, Toggle }
