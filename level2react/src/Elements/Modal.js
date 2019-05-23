640f8b064308af62cd3f1223cb36b096b9c14634
Unknown <sarah4594@gmail.com>
2019-05-23 00:02:21 -0500

    Move Modal into Elements folder, fix imports, and add styling to card template

--- level2react/src/Elements/Modal.js
+++ level2react/src/Elements/Modal.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import Portal from './Portal'
+import { Portal } from 'Utilities'
+import { Card } from './Cards'
 import Icon from './Icon'
 
 export default class Modal extends Component {
@@ -34,14 +35,10 @@ const ModalWrapper = styled.div`
   justify-content: center;
   align-items: center;
 `
-const ModalCard = styled.div`
+const ModalCard = styled(Card)`
   position: relative;
-  background: white;
-  border-radius: 5px;
-  padding: 15px;
   min-width: 320px;
   z-index: 10;
-  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.3);
   margin-bottom: 100px;
 `
 const CloseButton = styled.button`
