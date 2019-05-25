9bf52909aada739090545232ae88b92fad1ed7ee
sarah4594 <sarah4594@gmail.com>
2019-05-25 11:05:34 -0500

    Use new utility components to simplify Modal

--- level2react/src/Elements/Modal.js
+++ level2react/src/Elements/Modal.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import { Portal } from 'Utilities'
+import { Portal, absolute } from 'Utilities'
 import { Card } from './Cards'
 import Icon from './Icon'
 
@@ -26,9 +26,7 @@ export default class Modal extends Component {
 }
 
 const ModalWrapper = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
+  ${absolute({})};
   width: 100%;
   height: 100%;
   display: flex;
@@ -42,19 +40,18 @@ const ModalCard = styled(Card)`
   margin-bottom: 100px;
 `
 const CloseButton = styled.button`
-  position: absolute;
-  top: 0;
-  right: 0;
+  ${absolute({
+    y: 'top',
+    x: 'right'
+  })};
   border: none;
   background: transparent;
   padding: 10px;
 `
 const Background = styled.div`
-  position: absolute;
+  ${absolute({})};
   width: 100%;
   height: 100%;
-  top: 0;
-  left: 0;
   background: black;
   opacity: 0.5;
 `
