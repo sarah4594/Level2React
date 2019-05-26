0df657b53880e9582d0d5b91e9d48900122a35fd
sarah4594 <sarah4594@gmail.com>
2019-05-26 14:10:56 -0500

    Create user context consumer and provider

--- level2react/src/App.js
+++ level2react/src/App.js
@@ -3,26 +3,51 @@ import logo from './logo.svg'
 import './App.css'
 import { Toggle } from 'Utilities'
 import { Modal } from 'Elements'
+import User from './User'
+import { UserContext } from './UserContext'
+
+class UserProvider extends Component {
+  state = {
+    id: '123',
+    name: 'Sarah',
+    email: 'sarah4594@gmail.com'
+  }
+
+  render() {
+    return (
+      <UserContext.Provider
+        value={{
+          user: this.state
+        }}
+      >
+        {this.props.children}
+      </UserContext.Provider>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Toggle>
-          {({ on, toggle }) => (
-            <Fragment>
-              <button onClick={toggle}>Login</button>
-              <Modal on={on} toggle={toggle}>
-                <h1>What's up</h1>
-              </Modal>
-            </Fragment>
-          )}
-        </Toggle>
-      </div>
+      <UserProvider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <User />
+          <Toggle>
+            {({ on, toggle }) => (
+              <Fragment>
+                <button onClick={toggle}>Login</button>
+                <Modal on={on} toggle={toggle}>
+                  <h1>What's up</h1>
+                </Modal>
+              </Fragment>
+            )}
+          </Toggle>
+        </div>
+      </UserProvider>
     )
   }
 }
