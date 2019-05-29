b3bdc997cb01d2bc6b0dd7b746249165098c4c76
sarah4594 <sarah4594@gmail.com>
2019-05-28 23:02:45 -0500

    Style show/hide toggle using react spring

--- level2react/src/Elements/Cards.js
+++ level2react/src/Elements/Cards.js
@@ -5,11 +5,10 @@ export const Card = styled.div`
   background: white;
   border-radius: 5px;
   padding: 15px;
-  color: ${colors.purp};
+  color: ${colors.black};
   ${elevation[4]};
   ${transition({
-    ease: 'ease-in',
-    length: '1s'
+    property: 'box-shadow'
   })};
   &:hover {
     ${elevation[5]}
