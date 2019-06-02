4a2bed899393feacd6b41939eef538628c64273a
sarah4594 <sarah4594@gmail.com>
2019-06-01 22:27:56 -0500

    Use spring native to clean up animation

--- level2react/src/Drag.js
+++ level2react/src/Drag.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import { Gesture } from 'react-with-gesture'
 import { Spring, animated, interpolate } from 'react-spring'
 import { Card } from 'Elements'
+
+const AnimCard = Card.withComponent(animated.div)
 
 export default class Drag extends Component {
   render() {
@@ -9,19 +12,20 @@ export default class Drag extends Component {
       <Gesture>
         {({ down, xDelta }) => (
           <Spring
+            native
             to={{
               x: down ? xDelta : 0
             }}
             immediate={name => down && name === 'x'}
           >
             {({ x }) => (
-              <Card
+              <AnimCard
                 style={{
-                  transform: `translatex(${x}px)`
+                  transform: x.interpolate(x => `translatex(${x}px)`)
                 }}
               >
                 <h1>Drag Me</h1>
-              </Card>
+              </AnimCard>
             )}
           </Spring>
         )}
